6b24d2dbaba2eecef4c4ea1f309484331df78bda
Kevem Lennisk Almeida Andrade <kandrade@MacBook-Pro-de-Kevem.local>
2021-05-17 23:58:43 -0300

    Refactor hello world package name

---
+++ src/academy/lernprogramming/helloworld/Main.kt
@@ -0,0 +1,5 @@
+package academy.lernprogramming.helloworld
+
+fun main(args: Array<String>) {
+    print("Hi there")
+}
--- src/br/kotlintojavadevelopers/Main.kt
+++
@@ -1,5 +0,0 @@
-package br.kotlintojavadevelopers
-
-fun main() {
-    print("Hi there")
-}
